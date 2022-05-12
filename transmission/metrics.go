7d17f019ee17ae5464babd655797eabd5a88a4a7
sujitha-pallapothu <sujitha.pallapothu@opsramp.com>
2022-05-12 17:34:44 +0530

    change4

--- transmission/metrics.go
+++ transmission/metrics.go
@@ -2,13 +2,14 @@ package transmission
 
 // Metrics is an interface that can be fulfilled by something like statsd
 type Metrics interface {
+	Register(name string, metricType string)
 	Gauge(string, interface{})
 	Increment(string)
 	Count(string, interface{})
 }
 
 type nullMetrics struct{}
-
+func (nm *nullMetrics) Register(string, string) {}
 func (nm *nullMetrics) Gauge(string, interface{}) {}
 func (nm *nullMetrics) Increment(string)          {}
 func (nm *nullMetrics) Count(string, interface{}) {}
