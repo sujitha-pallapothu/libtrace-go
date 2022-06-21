a64edf49eafeb24a845303184f61b3c5a105dba7
sujitha-pallapothu <sujitha.pallapothu@opsramp.com>
2022-06-21 15:56:23 +0530

    proxy change2

--- transmission/transmission.go
+++ transmission/transmission.go
@@ -431,8 +431,8 @@ func (b *batchAgg) exportProtoMsgBatch(events []*Event) {
 			//fmt.Println("proxyurl",proxyurl)
             os.Getenv("HTTP_PROXY")
 			fmt.Println("htps",os.Getenv("HTTP_PROXY"))
-			//conn, err = grpc.Dial(apiHostUrl, grpc.WithTransportCredentials(tlsCreds))
-			conn, err = grpc.Dial(os.Getenv("HTTPS_PROXY"), grpc.WithTransportCredentials(tlsCreds))
+			conn, err = grpc.Dial(apiHostUrl, grpc.WithTransportCredentials(tlsCreds))
+		//	conn, err = grpc.Dial(os.Getenv("HTTPS_PROXY"), grpc.WithTransportCredentials(tlsCreds))
 
 
 			if err != nil {
